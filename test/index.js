0038c3e460a47c9f46173ddd7f32371da5ec9061
Jro <bobo999bobo@outlook.com>
2018-07-29 11:08:31 +0800

    Add more tests

--- test/index.js
+++ test/index.js
@@ -10,15 +10,101 @@ function testShift(msg,key){
   return msg == crypto.shift(crypto.shift(msg,key),-key)
 }
 
+function testShiftInvalidChar(alphaNum,string,key){
+  let text = alphaNum+string
+  if(text.length==0||/^[a-zA-Z]+$/.test(text)){
+    //This test case is invalid, skip
+    return true
+  }
+  if(text.toUpperCase().length!=text.length){
+    //Another invalid test case
+    //For example
+    // "ßC".toUpperCase()
+    // => "SSC"
+    return true
+  }
+  text = text.toUpperCase()
+  try{
+    const encrypted = crypto.shift(text,key)
+    return false
+  }catch(e){
+    try{
+      const decrypted = crypto.shift(text,-key)
+      return false
+    }catch(e){
+      return true
+    }
+  }
+}
+
 function testIntEncode(msg){
   msg = msg.toUpperCase()
-  return msg == crypto.intEncode(crypto.intEncode(msg,true),false).join("")
+  const multiplier = 7//parseInt(Math.random().toString()[6])
+  const encoded = crypto.intEncode(msg,true)
+
+  for(let i = 0 ;i<encoded.length;i++){
+    encoded[i] += 37 * multiplier
+  }
+  return msg == crypto.intEncode(encoded,false).join("")
+}
+
+function testIntEncodeInvalidChar(alphaNum,string){
+  let text = alphaNum+string
+  if(text.length==0||text.includes(" ")||/^[a-z0-9]+$/i.test(text)){
+    //This test case is invalid, skip
+    return true
+  }
+  if(text.toUpperCase().length!=text.length){
+    //Another invalid test case
+    //For example
+    // "ßC".toUpperCase()
+    // => "SSC"
+    return true
+  }
+  text = text.toUpperCase()
+  try{
+    const encrypted = crypto.intEncode(text,true)
+    return false
+  }catch(e){
+    try{
+      const array = text.split(",")
+      for(const part of array){
+        if(!isNaN(parseInt(part))){
+          //Another invalid test case
+          return true
+        }
+      }
+      const decrypted = crypto.intEncode(array,false)
+      return false
+    }catch(e){
+      return true
+    }
+  }
 }
 
 function testHillKeygen(keyLength){
   const key = crypto.hillKeygen(keyLength)
   const determinant = crypto.determinant(key)
   return determinant === 1
+}
+function testHillCipherUnevenText(key){
+  return text=>{
+    text = text.toUpperCase()
+    if(!(text.length%2)){
+      text+="A"
+    }
+    try{
+      const encrypted = crypto.hillCipherEncrypt(text,key,false)
+      return false
+    }catch(e){
+      try{
+        const decrypted = crypto.hillCipherDecrypt(text,key,false)
+        return false
+      }catch(e){
+        return true
+      }
+    }
+  }
 }
 function testHillCipher(text,keyLength){
   text = text.toUpperCase()
@@ -51,11 +137,37 @@ describe("Crypto",function(){
     }
     expect(result.result).to.be.true
   })
+  it("Should be able to detect and reject invalid characters in shift cipher",function(){
+    const result = check(property(
+      gen.alphaNumString,
+      gen.string,
+      gen.int,
+      testShiftInvalidChar),
+      { numTests: 1000 }
+    )
+    if(!result.result){
+      console.log(result)
+    }
+    expect(result.result).to.be.true
+  })
 
   it("Should be able to use the integer encoding/decoding",function(){
     const result = check(property(
       gen.alphaNumString,
       testIntEncode),
+      { numTests: 1000 }
+    )
+    if(!result.result){
+      console.log(result)
+    }
+    expect(result.result).to.be.true
+  })
+
+  it("Should be able to detect and reject invalid characters in integer encoding",function(){
+    const result = check(property(
+      gen.alphaNumString,
+      gen.string,
+      testIntEncodeInvalidChar),
       { numTests: 1000 }
     )
     if(!result.result){
@@ -76,18 +188,38 @@ describe("Crypto",function(){
     }
     expect(result.result).to.be.true
   })
-
+  
   it("Should be able to encrypt and decrypt using hill cipher",function(){
     this.timeout(5000)
     const result = check(property(
       gen.alphaNumString,
       gen.intWithin(2,6),
       testHillCipher),
-      { numTests: 1000 }
+      { numTests: 500 }
     )
     if(!result.result){
       console.log(result)
     }
     expect(result.result).to.be.true
+  })
+
+  it("Should be able to detect and reject errors",function(){
+    this.timeout(5000)
+    const key = crypto.hillKeygen(5)
+    const result = check(property(
+      gen.alphaNumString,
+      testHillCipherUnevenText(key)),
+      { numTests: 500 }
+    )
+    if(!result.result){
+      console.log(result)
+    }
+    expect(result.result).to.be.true
+    const badFn = ()=>{
+      const key = crypto.hillKeygen(5)
+      key[0][0] = key[0][0]+100
+      return crypto.hillCipherDecrypt("hi",key,false)
+    }
+    expect(badFn).to.throw()
   })
 })
